4c12525b70a480acf145b1fe7b2bd54ed844cd91
dr.Edelon <Denys.Pankrushev@gmail.com>
2025-12-05 22:01:14 +0200

    cleanup after GPT

--- GNU_MAKE/features/fh_MCU_platform.mk
+++ GNU_MAKE/features/fh_MCU_platform.mk
@@ -1,29 +1,29 @@
 DST_PLATFORM := STM32
 
-$(eval PLATFORM_DIR = $(SRC_DIR)/platform) $(eval INCLUDES += $(PLATFORM_DIR)/API)
+$(eval PLATFORM_DIR = $(SRC_DIR)/platform) $(eval INCLUDES += $(PLATFORM_DIR)) $(eval INCLUDES += $(PLATFORM_DIR)/API)
     $(eval MCU_DIR = $(PLATFORM_DIR)/MCU) $(eval INCLUDES += $(MCU_DIR))
-        $(eval STM32_DIR = $(MCU_DIR)/STM32F7) $(eval INCLUDES += $(STM32_DIR))
-            $(eval FS_DIR = $(STM32_DIR)/FileSystem) $(eval INCLUDES += $(FS_DIR))
-                SRC_LIST += $(FS_DIR)/per_SD_tools.c
-                SRC_LIST += $(FS_DIR)/SD_TF_tools.c
-                SRC_LIST += $(FS_DIR)/MBR_tool.c
-                SRC_LIST += $(FS_DIR)/fs_FAT32.c
+        $(eval FS_DIR = $(MCU_DIR)/FileSystem) $(eval INCLUDES += $(FS_DIR))
+            SRC_LIST += $(FS_DIR)/per_SD_tools.c
+            SRC_LIST += $(FS_DIR)/SD_TF_tools.c
+            SRC_LIST += $(FS_DIR)/MBR_tool.c
+            SRC_LIST += $(FS_DIR)/fs_FAT32.c
 
-#             SRC_LIST += $(STM32_DIR)/main.c
+        $(eval STM32_DIR = $(MCU_DIR)/STM32F7) $(eval INCLUDES += $(STM32_DIR))
+            # SRC_LIST += $(STM32_DIR)/main.c
             SRC_LIST += $(STM32_DIR)/perepherial.c
             SRC_LIST += $(STM32_DIR)/retarget_io.c
             SRC_LIST += $(STM32_DIR)/per_FMC_SDRAM.c
 
-        SRC_LIST += $(MCU_DIR)/sys_STM32F7.c
+        SRC_LIST += $(MCU_DIR)/sys_mcu.c
+        SRC_LIST += $(MCU_DIR)/sysFS_mcu.c
+        SRC_LIST += $(MCU_DIR)/sysIO_mcu.c
 
-$(eval PLATFORM_DIR = $(SRC_DIR)/platform) $(eval INCLUDES += $(PLATFORM_DIR)) $(eval INCLUDES += $(PLATFORM_DIR)/API)
-    $(eval MCU_DIR = $(PLATFORM_DIR)/MCU) $(eval INCLUDES += $(MCU_DIR))
-#         SRC_LIST += $(MCU_DIR)/cd_null.c
-#         SRC_LIST += $(MCU_DIR)/snd_null.c
-#         SRC_LIST += $(MCU_DIR)/sys_null.c
-#         SRC_LIST += $(MCU_DIR)/vid_null.c
+        # SRC_LIST += $(MCU_DIR)/cd_null.c
+        # SRC_LIST += $(MCU_DIR)/snd_null.c
+        # SRC_LIST += $(MCU_DIR)/sys_null.c
+        # SRC_LIST += $(MCU_DIR)/vid_null.c
         SRC_LIST += $(MCU_DIR)/net_mcu.c
-#         SRC_LIST += $(MCU_DIR)/in_mcu.c
+        # SRC_LIST += $(MCU_DIR)/in_mcu.c
 
 # $(eval ROOT_DIR := $(STMSRC_DIR)) $(eval INCLUDES += $(ROOT_DIR))
 
--- src/platform/MCU/snd_null.c
+++
@@ -1,6 +0,0 @@
-void SNDDMA_Init() {}
-int SNDDMA_GetDMAPos() { return 0; }
-void SNDDMA_Shutdown() {}
-void SNDDMA_Submit() {}
-void SNDDMA_BeginPainting() {}
-void SNDDMA_EndPainting() {}
