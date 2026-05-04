b5f6517a006705470ce81e6969c1553a19e3670b
dr.Edelon <Denys.Pankrushev@gmail.com>
2026-05-03 23:52:39 -0600

    some rename

--- GNU_MAKE/features/fh_qRenderOpenGL.mk
+++ GNU_MAKE/features/fh_qRenderOpenGL.mk
@@ -2,21 +2,6 @@ DEFINES += GLQUAKE
 CFLAGS  += -I/opt/X11/include
 LDFLAGS += -L/opt/X11/lib -lGL -framework OpenGL
 
-                $(eval VID_DIR = $(CL_SIDE_DIR)/video) $(eval INCLUDES += $(VID_DIR))
-                        $(eval OPENGL_DIR = $(VID_DIR)/gl) $(eval INCLUDES += $(OPENGL_DIR))
-#                                 SRC_LIST += $(OPENGL_DIR)/gl_vidlinux.c
-#                                 SRC_LIST += $(OPENGL_DIR)/gl_vidlinuxglx.c
-                                SRC_LIST += $(OPENGL_DIR)/vid_x11gl.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_screen.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_draw.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_rmain.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_rmisc.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_rsurf.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_rlight.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_refrag.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_warp.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_model.c
-                                SRC_LIST += $(OPENGL_DIR)/gl_mesh.c
 
                         $(eval DRAW_DIR = $(VID_DIR)/draw) $(eval INCLUDES += $(DRAW_DIR))
 #                                 SRC_LIST += $(DRAW_DIR)/nonintel.c
@@ -37,17 +22,34 @@ LDFLAGS += -L/opt/X11/lib -lGL -framework OpenGL
 #                                 SRC_LIST += $(RENDER_DIR)/r_aclip.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_alias.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_bsp.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_light.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_draw.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_efrag.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_edge.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_misc.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_main.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_sky.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_sprite.c
-#                                 SRC_LIST += $(RENDER_DIR)/r_surf.c
                                 SRC_LIST += $(RENDER_DIR)/r_part.c
 #                                 SRC_LIST += $(RENDER_DIR)/r_vars.c
+
+                $(eval VID_DIR = $(CL_SIDE_DIR)/video) $(eval INCLUDES += $(VID_DIR))
+                        $(eval OPENGL_DIR = $(VID_DIR)/gl) $(eval INCLUDES += $(OPENGL_DIR))
+#                                 SRC_LIST += $(OPENGL_DIR)/gl_vidlinux.c
+#                                 SRC_LIST += $(OPENGL_DIR)/gl_vidlinuxglx.c
+                                SRC_LIST += $(OPENGL_DIR)/vid_x11gl.c
+
+                                SRC_LIST += $(OPENGL_DIR)/gl_screen.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_draw.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_draw.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_main.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_rmain.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_misc.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_rmisc.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_surf.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_rsurf.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_light.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_rlight.c
+#                                 SRC_LIST += $(RENDER_DIR)/r_efrag.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_refrag.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_warp.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_model.c
+                                SRC_LIST += $(OPENGL_DIR)/gl_mesh.c
 
         $(eval SHARED_DIR = $(ENG_DIR)/Shared) $(eval INCLUDES += $(SHARED_DIR))
                 $(eval AST_DIR = $(SHARED_DIR)/assets) $(eval INCLUDES += $(AST_DIR))
