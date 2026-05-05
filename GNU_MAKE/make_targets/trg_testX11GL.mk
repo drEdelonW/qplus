6c46945d9049236d8673d8a4ae56f8e841b18803
dr.Edelon <Densy.Pankrushev@gmail.com>
2026-05-05 02:27:15 +0100

    test OpenGL run under LINUX

--- GNU_MAKE/make_targets/trg_testX11GL.mk
+++ GNU_MAKE/make_targets/trg_testX11GL.mk
@@ -7,11 +7,14 @@ $(eval X11GL_DIR = $(SRC_DIR)/testX11GL) $(eval INCLUDES += $(X11GL_DIR))
 #----------LINUX------------
 ifeq ($(UNAME_S),Linux)
 
+    RUN_PREFIX := DISPLAY=:1
+
+    LDLIBS += -lGL -lX11 -lm
 
 #----------MacOS------------
 else ifeq ($(UNAME_S),Darwin)
 
-    HOMEBREW_PREFIX := $(shell brew --prefix 2>/dev/null)
+#     HOMEBREW_PREFIX := $(shell brew --prefix 2>/dev/null)
     CFLAGS  += -arch arm64
     CFLAGS  += -I/opt/X11/include
 #     CXXFLAGS += -I$(HOMEBREW_PREFIX)/include
